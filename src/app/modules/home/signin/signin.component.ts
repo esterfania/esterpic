67506267715c0cee08f7b2abff97c8114445d329
Ester <esterfania@users.noreply.github.com>
2020-08-18 18:09:41 -0300

    Inclui FormBuilder no SignInComponent

--- src/app/modules/home/signin/signin.component.ts
+++ src/app/modules/home/signin/signin.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit {
-    constructor(private loginForm: FormGroup) { }
+    loginForm: FormGroup
+    
+    constructor(private formBuilder: FormBuilder) { }
     
     ngOnInit(): void {
         throw new Error("Method not implemented.");
