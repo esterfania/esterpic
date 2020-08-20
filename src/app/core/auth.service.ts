acedfaa0940a0542488f0421fae1d93aaa9704db
Ester <esterfania@users.noreply.github.com>
2020-08-20 18:05:48 -0300

    Cria authService

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(user: User): Observable<User> {
+    return this.http.post<User>(API_URL + 'user/login', user)
+  }
+}
