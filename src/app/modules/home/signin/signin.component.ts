f6f2ea36a9c35217e0307e90bbcc745d03e7dbc0
Ester <esterfania@users.noreply.github.com>
2020-08-20 18:06:20 -0300

    Injeta authService em SigInComponent e cria o método login

--- src/app/modules/home/signin/signin.component.ts
+++ src/app/modules/home/signin/signin.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit {
-    
+
     loginForm: FormGroup
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             username: ['', [Validators.required]],
-            password: ['', [Validators.required, Validators.minLength(8)]]
+            password: ['', [Validators.required]]
         });
+    }
+
+    login(): void {
+        let user: User = {
+            userName: this.loginForm.get('username').value,
+            password: this.loginForm.get('password').value
+        };
+
+        this.authService
+            .authenticate(user)
+            .subscribe(res => console.log(res),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset()
+                });
     }
 
 }
