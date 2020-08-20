afbebeb9039cf999f4f17f320b74b43ca4fe666f
Ester <esterfania@users.noreply.github.com>
2020-08-20 15:50:33 -0300

    Inclui MessaModule em HomeModule

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,12 +3,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 
 import { SigninComponent } from "./signin/signin.component";
+import { MessageModule } from "src/app/shared";
 
 @NgModule({
     declarations: [SigninComponent],
     imports: [
+        MessageModule,
         CommonModule,
-        ReactiveFormsModule]
+        ReactiveFormsModule,
+    ]
 })
 export class HomeModule {
 
