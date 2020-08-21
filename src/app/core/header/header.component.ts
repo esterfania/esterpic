67a58903b9332b70f598770ccc640da0bb1ef084
Ester <esterfania@users.noreply.github.com>
2020-08-21 18:13:04 -0300

    Chamada de UserService em header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,23 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
+import { UserService } from "../user/user.service";
+import { User } from "../user/user";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
-    @Input() user: string;
+    user$: Observable<User>;
+    user: User;
+
+    constructor(private userService: UserService) {
+        this.getUser();
+    }
+
+    getUser() {
+        this.user$ = this.userService.getUser();
+        this.user$
+            .subscribe(res => this.user = res);
+    }
 }
