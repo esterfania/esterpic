6cef15c60ede5ef4a2d0e388f5d06f925d736942
Ester <esterfania@users.noreply.github.com>
2020-08-22 17:46:07 -0300

    Insere direcionamento no link do header

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -19,7 +19,6 @@ const routes: Routes = [
     {
         path: 'user/:userName',
         component: PicutresListComponent,
-        canActivate: [UserGuard],
         resolve: {
             pictures: PicturesListResolver
         }
