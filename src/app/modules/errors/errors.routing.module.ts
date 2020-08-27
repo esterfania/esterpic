0adaafe7190d7751714f54636c5fdc40bee7b263
Ester <esterfania@users.noreply.github.com>
2020-08-26 22:13:08 -0300

    Inclui alteração do title de acordo com a rota

--- src/app/modules/errors/errors.routing.module.ts
+++ src/app/modules/errors/errors.routing.module.ts
@@ -7,7 +7,10 @@ const routes: Routes = [
     
     {
         path: '',
-        component: NotFoundComponent
+        component: NotFoundComponent,
+        data: {
+            title: 'Not found'
+        }
     }
 
 ];
