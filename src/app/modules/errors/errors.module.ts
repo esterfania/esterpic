f083abacee253c0e54f2f80efbfd90db497fc7c1
Ester <esterfania@users.noreply.github.com>
2020-08-29 21:23:36 -0300

    Inclui GlobalErrorHandler

--- src/app/modules/errors/errors.module.ts
+++ src/app/modules/errors/errors.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorsRoutingModule } from './errors.routing.module';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   imports: [
     CommonModule,
     ErrorsRoutingModule
   ],
-  declarations: [NotFoundComponent]
+  declarations: [NotFoundComponent],
+  providers:[{
+    provide: ErrorHandler,
+    useClass: GlobalErrorHandler
+  }]
 })
 export class ErrorsModule { }
